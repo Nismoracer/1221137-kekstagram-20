5b7afa0ab3da006956e2730eadd6d59c585138b0
Илья Грунов <ilya.grunov@gmail.com>
2020-07-13 20:42:05 +0300

    Добавлена фильтрация элементов по нажатию кнопки.

--- source/js/main.js
+++ source/js/main.js
@@ -2,7 +2,9 @@
 
 (function () {
 
-  var similarListElement = document.querySelector('.pictures');
+  var filtersSection = document.querySelector('.img-filters');
+  var filtersForm = document.querySelector('.img-filters__form');
+  var uploadSection = document.querySelector('.img-upload');
 
   var similarElementTemplate = document.querySelector('#picture')
     .content
@@ -20,16 +22,62 @@
     return currentPhoto;
   };
 
+  var onFiltersClick = function (evt) {
+    var filterButtons = document.querySelectorAll('.img-filters__button');
+
+    for (var i = 0; i < filterButtons.length; i++) {
+      if (filterButtons[i].classList.contains('img-filters__button--active')) {
+        filterButtons[i].classList.remove('img-filters__button--active');
+      }
+    }
+
+    evt.target.classList.add('img-filters__button--active');
+
+    switch (evt.target.id) {
+      case 'filter-default':
+        var cbDefault = window.util.debounce(function () {
+          renderPhotosList(window.transmit.receivedPhotos);
+        });
+        cbDefault(window.transmit.receivedPhotos);
+        break;
+      case 'filter-random':
+        var cbRandom = window.util.debounce(function () {
+          window.data.chooseRandomPhotos(renderPhotosList);
+        });
+        cbRandom(window.transmit.receivedPhotos);
+        break;
+      case 'filter-discussed':
+        var cbPopular = window.util.debounce(function () {
+          window.data.sortFromPopularToNot(renderPhotosList);
+        });
+        cbPopular(renderPhotosList);
+        break;
+    }
+  };
+
   var renderPhotosList = function (photosArray) {
+    var similarListElement = document.querySelector('.pictures');
+
+    while (similarListElement.lastChild !== uploadSection) {
+      similarListElement.removeChild(similarListElement.lastChild);
+    }
+
     var fragment = document.createDocumentFragment();
 
     for (var i = 0; i < photosArray.length; i++) {
       fragment.appendChild(renderPhotoElement(photosArray[i]));
     }
+
     similarListElement.appendChild(fragment);
+
   };
 
-  window.transmit(renderPhotosList, window.popup.errorUpload, 'receive');
+
+  window.transmit.exchange(renderPhotosList, window.popup.errorUpload, 'receive');
+  filtersSection.classList.remove('img-filters--inactive');
+  filtersForm.addEventListener('click', function (evt) {
+    onFiltersClick(evt);
+  });
 
   uploadWindowOpen.addEventListener('change', window.modal.openFilters);
 
