b07aceb2b33ce8687816218c664b395bfc78294d
Илья Грунов <ilya.grunov@gmail.com>
2020-07-17 14:14:35 +0300

    Исправлен путь нахождения папки js для линтера.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
-"use strict";
+'use strict';
 
-var gulp = require("gulp");
-var server = require("browser-sync").create();
+var gulp = require('gulp');
+var server = require('browser-sync').create();
 
-gulp.task("server", function () {
+gulp.task('server', function () {
   server.init({
-    server: "source/",
+    server: '/',
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch('*.html').on('change', server.reload);
 });
 
-gulp.task("start", gulp.series("server"));
+gulp.task('start', gulp.series('server'));
